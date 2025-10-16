3abcc9f8f1ab58d18a133004e7b7d2960e1c2045
Mauricio Ramirez <165215692+MauricioRamirezzz@users.noreply.github.com>
2025-10-16 16:58:32 -0300

    Fix indentation and formatting in es_email_valido

--- Backend/utilidades.py
+++ Backend/utilidades.py
@@ -4,5 +4,4 @@ def es_email_valido(email: str) -> bool:
     #Verifica si el formato del email es válido (debe tener exactamente un @ y no espacios).
     if email.count("@") != 1 or email.count(" ") > 0:
         return False
-
     return True
