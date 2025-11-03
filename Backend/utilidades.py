b4d5df0aba48caec0c6368e75e585bc5d03ca2b8
Lara <165215053+larazobaji@users.noreply.github.com>
2025-11-03 20:23:42 -0300

    Delete Backend directory

--- Backend/utilidades.py
+++
@@ -1,7 +0,0 @@
-from __future__ import annotations
-
-def es_email_valido(email: str) -> bool:
-    #Verifica si el formato del email es válido (debe tener exactamente un @ y no espacios).
-    if email.count("@") != 1 or email.count(" ") > 0:
-        return False
-    return True
